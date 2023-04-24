d61a3f33a514a8608ff166358fbc4631b834fc50
belizario87 <leonardobelizario88@gmail.com>
2023-04-24 16:18:24 -0300

    add entity classes

---
+++ demo-mvc/src/main/java/com/mballem/curso/boot/demomvc/domain/Cargos.java
@@ -0,0 +1,51 @@
+package com.mballem.curso.boot.demomvc.domain;
+
+import java.util.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "CARGOS")
+public class Cargos extends AbstractEntity<Long> {
+
+    @Column(name = "nome", nullable = false, unique = true, length = 60)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "id_departamento_fk")
+    private Departamento departamento;
+
+    @OneToMany(mappedBy = "cargos")
+    private List<Funcionarios> funcionarios;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public List<Funcionarios> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionarios> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+
+}
---
+++ demo-mvc/src/main/java/com/mballem/curso/boot/demomvc/domain/Departamento.java
@@ -0,0 +1,36 @@
+package com.mballem.curso.boot.demomvc.domain;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "DEPARTAMENTOS")
+public class Departamento extends AbstractEntity<Long> {
+
+    @Column(name = "nome", nullable = false, unique = true, length = 60)
+    private String nome;
+
+    @OneToMany(mappedBy = "departamentos")
+    private List<Cargos> cargos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Cargos> getCargos() {
+        return cargos;
+    }
+
+    public void setCargos(List<Cargos> cargos) {
+        this.cargos = cargos;
+    }
+
+}
